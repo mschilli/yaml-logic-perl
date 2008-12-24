3214c36065e7decb09fb19d3ee38b496eb5c3ec9
mschilli <github@perlmeister.com>
2008-12-24 20:34:27 +0000

    upd

--- Logic.pm
+++ Logic.pm
@@ -4,6 +4,8 @@ package YAML::Logic;
 
 use strict;
 use warnings;
+use Log::Log4perl qw(:easy);
+use Template;
 
 our $VERSION = "0.01";
 
@@ -19,17 +21,47 @@ sub new {
     bless $self, $class;
 }
 
+###########################################
+sub interpolate {
+###########################################
+    my($self, $input, $vars) = @_;
+
+    return $input if $input !~ /^\$/;
+
+    my $out;
+    my $template = Template->new();
+
+    $input =~ s/\$(\S+)/[%- $1 %]/g;
+
+    $template->process( \$input, $vars, \$out ) or
+        LOGDIE $template->error();
+
+    return $out;
+}
+
 1;
 
 __END__
 
 =head1 NAME
 
-YAML::Logic - blah blah blah
+YAML::Logic - Simple boolean logic in YAML
 
 =head1 SYNOPSIS
 
+    use YAML qw(Load);
     use YAML::Logic;
+
+    my $data = Load(q{
+      # is $var equal to "foo"?
+    expr:
+      - $var
+      - foo
+    };
+
+    if( YAML::Logic::eval( $data->{expr}, { var => "foo" }) ) {
+        print "True!\n";
+    }
 
 =head1 DESCRIPTION
 
@@ -42,14 +74,14 @@ The variable is set to "foo".
 =for test "yaml" begin
 
     expr:
-      - '$var'
+      - $var
       - foo
 
 =for test "yaml" end
 
 =item *
 
-The variable is I<not> set to "foo".
+The variable is not set to "foo".
 
 =for test "yaml" begin
 
@@ -61,7 +93,7 @@ The variable is I<not> set to "foo".
 
 =item *
 
-The variable is I<not> set to "foo" and I<not> set to "bar".
+The variable is not set to "foo" and not set to "bar".
 
 =for test "yaml" begin
 
@@ -75,16 +107,71 @@ The variable is I<not> set to "foo" and I<not> set to "bar".
 
 =item *
 
-The variable is set to "foo" I<or> set to "bar".
+The variable is set to "foo" or set to "bar".
 
 =for test "yaml" begin
 
     expr:
       - or
+      -
         - '$var'
         - foo
         - '$var'
         - bar
+
+=for test "yaml" end
+
+=item *
+
+The variable is set to "foo" and not set to "bar".
+
+=for test "yaml" begin
+
+    expr:
+        - '$var'
+        - foo
+        - '!$var'
+        - bar
+
+=for test "yaml" end
+
+=item *
+
+The variable matches the regular expression /^foo.*/
+
+=for test "yaml" begin
+
+    expr:
+        - '$var'
+        - like: "^foo.*"
+
+=for test "yaml" end
+
+=item *
+
+The variable matches both regular expressions, /^foo.*/ and /^bar.*/.
+
+=for test "yaml" begin
+
+    expr:
+        - '$var'
+        -
+          - like: "^foo.*"
+          - like: "^bar.*"
+
+=for test "yaml" end
+
+=item *
+
+The variable matches neither /^foo.*/ nor /^bar.*/.
+
+=for test "yaml" begin
+
+    expr:
+        - '!$var'
+        -
+          - like: "^foo.*"
+          - like: "^bar.*"
 
 =for test "yaml" end
 
---
+++ t/002Interpol.t
@@ -0,0 +1,18 @@
+######################################################################
+# Test suite for YAML::Logic
+# by Mike Schilli <cpan@perlmeister.com>
+######################################################################
+use warnings;
+use strict;
+use YAML qw(Load Dump);
+use YAML::Logic;
+use Test::More qw(no_plan);
+use Data::Dumper;
+
+my $logic = YAML::Logic->new();
+
+my $out = $logic->interpolate( '$var', { var => "foo" } );
+is($out, "foo", "simple variable interpolation");
+
+$out = $logic->interpolate( '$var.field', { var => { field => "foo" } } );
+is($out, "foo", "hash entry");
